a52fb50bbfade03585508a652078764ff1a028b3
Dmitriy Vorobey <dvorobey@qulix.com>
2022-03-22 21:06:20 +0300

    fix HomeController.cs

--- src/Chess.Identity/Chess.Identity/Controllers/HomeController.cs
+++ src/Chess.Identity/Chess.Identity/Controllers/HomeController.cs
@@ -32,15 +32,7 @@ namespace Chess.Identity.Controllers
 
         public IActionResult Index()
         {
-            if (_environment.IsDevelopment())
-            {
-                // only show in development
-                return View();
-            }
-
-            _logger.LogInformation("Homepage is disabled in production. Returning 404.");
-
-            return NotFound();
+            return View();
         }
 
         /// <summary>
